4a0a949d26191de9116a940cd2aca26cad92e83f
yosrazayani <yosra.zayani@esprit.tn>
2024-07-05 19:59:11 +0200

    my commit

---
+++ ConfigS/src/main/java/com/example/demo/ConfigSApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigSApplication.class, args);
+	}
+
+}
